53b4395326dc929e626db22265fd983093b8ccd2
joseayala <joseayala45@outlook.es>
2024-09-20 23:18:09 -0500

    feat: set up database connection.

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"cash_register/internal/infrastructure/database"
+	"log"
+)
+
 func main() {
+	//iniciar la conexion a la base de datos
+	database.InitDB()
+
+	db := database.GetDB()
+
+	// cerrar la conexion a la base de datos
+	defer func() {
+		if err := db.Close(); err != nil {
+			log.Fatalf("Error cerrando la conexion a la base de datos: %v", err)
+		}
+		log.Println("Conexion a la base de datos cerrada")
+	}()
 
 }
